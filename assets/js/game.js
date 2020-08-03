3499158c0a14fe3269c59b8830f84b18b9431092
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-03 16:51:38 -0600

    added js to the js file, enemy can attack the player

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,12 +8,45 @@ var playerAttack = 10;
 // Console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roberto";
-var playerHealth = 50;
-var playerAttack = 12;
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 // this creates a function named "fight"
 var fight = function() {
-    window.alert("Welcome to Robot Gladiators!")
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from the value of 'EnemyHealth' and use that reslut to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
 fight ();
