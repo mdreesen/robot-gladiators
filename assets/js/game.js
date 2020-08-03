792b89ca3e772aa21f4be6ac85503b1894ee7f10
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-03 14:00:45 -0600

    added and changed the js file to have player name and opponent name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,19 @@
-var playerName = window.prompt("What is your robot's name?");
-// Note the lack of qutatin marks around playerName
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// This will do math and log 20
-console.log(10 + 10);
-// What is this?
-console.log("Our robot's name is " + playerName);
+// window.alert("This is an alert! JavaScript is running!");
 
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+// Console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var playerHealth = 50;
+var playerAttack = 12;
 
 // this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begin!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!")
 }
 
-// fight ();
+fight ();
