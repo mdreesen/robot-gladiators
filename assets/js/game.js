86673f4577ba466e8d5f9d6ecd939b4aab227077
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-03 13:53:18 -0600

    added console logs to js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,11 @@
 var playerName = window.prompt("What is your robot's name?");
+// Note the lack of qutatin marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+// What is this?
+console.log("Our robot's name is " + playerName);
 
 window.alert("This is an alert! JavaScript is running!");
 
@@ -7,4 +14,4 @@ function fight() {
     window.alert("The fight has begin!");
 }
 
-fight ();
+// fight ();
